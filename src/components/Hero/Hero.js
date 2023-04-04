39757cd71651afbea8b77c9f68933ba290515382
clark <clark_mitchelljr@hotmail.com>
2023-04-04 11:39:10 -0700

    header-projects components added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,26 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a full stack web developer with a passion for creating beautiful
+        and functional websites. I am currently looking for a full time position
+        as a web developer.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location = "https://www.linkedin.com/in/cmitch06/")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
